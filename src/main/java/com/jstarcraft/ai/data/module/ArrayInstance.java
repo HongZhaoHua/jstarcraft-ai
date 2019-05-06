58f8137e24b322be18a7a19011270c137b2e8b4f
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-05-06 19:02:25 +0800

    数组与哈希实例支持拷贝实例.

--- src/main/java/com/jstarcraft/ai/data/module/ArrayInstance.java
+++ src/main/java/com/jstarcraft/ai/data/module/ArrayInstance.java
@@ -1,8 +1,8 @@
 package com.jstarcraft.ai.data.module;
 
-import com.jstarcraft.ai.data.QuantityAccessor;
 import com.jstarcraft.ai.data.DataInstance;
 import com.jstarcraft.ai.data.QualityAccessor;
+import com.jstarcraft.ai.data.QuantityAccessor;
 
 /**
  * 数组实例
@@ -12,135 +12,143 @@ import com.jstarcraft.ai.data.QualityAccessor;
  */
 public class ArrayInstance implements DataInstance {
 
-	/** 离散秩 */
-	private int qualityOrder;
+    /** 离散秩 */
+    private int qualityOrder;
 
-	/** 连续秩 */
-	private int quantityOrder;
+    /** 连续秩 */
+    private int quantityOrder;
 
-	/** 离散特征 */
-	private int[] qualityFeatures;
+    /** 离散特征 */
+    private int[] qualityFeatures;
 
-	/** 连续特征 */
-	private float[] quantityFeatures;
+    /** 连续特征 */
+    private float[] quantityFeatures;
 
-	/** 离散标记 */
-	private int qualityMark;
+    /** 离散标记 */
+    private int qualityMark;
 
-	/** 连续标记 */
-	private float quantityMark;
+    /** 连续标记 */
+    private float quantityMark;
 
-	/** 权重 */
-	private float weight;
+    /** 权重 */
+    private float weight;
 
-	public ArrayInstance(DataInstance instance) {
-		this.qualityOrder = instance.getQualityOrder();
-		this.quantityOrder = instance.getQuantityOrder();
-		this.qualityFeatures = new int[instance.getQualityOrder()];
-		{
-			for (int index = 0, size = instance.getQualityOrder(); index < size; index++) {
-				this.qualityFeatures[index] = DataInstance.defaultInteger;
-			}
-		}
-		this.quantityFeatures = new float[instance.getQuantityOrder()];
-		{
-			for (int index = 0, size = instance.getQuantityOrder(); index < size; index++) {
-				this.quantityFeatures[index] = DataInstance.defaultFloat;
-			}
-		}
-		instance.iterateQualityFeatures((index, value) -> {
-			this.qualityFeatures[index] = value;
-		});
-		instance.iterateQuantityFeatures((index, value) -> {
-			this.quantityFeatures[index] = value;
-		});
-		this.qualityMark = instance.getQualityMark();
-		this.quantityMark = instance.getQuantityMark();
-		this.weight = instance.getWeight();
-	}
+    public ArrayInstance(int qualityOrder, int quantityOrder) {
+        this.qualityOrder = qualityOrder;
+        this.quantityOrder = quantityOrder;
+        this.qualityFeatures = new int[qualityOrder];
+        this.quantityFeatures = new float[quantityOrder];
+        for (int index = 0; index < qualityOrder; index++) {
+            this.qualityFeatures[index] = DataInstance.defaultInteger;
+        }
+        for (int index = 0; index < quantityOrder; index++) {
+            this.quantityFeatures[index] = DataInstance.defaultFloat;
+        }
+        this.qualityMark = defaultInteger;
+        this.quantityMark = defaultFloat;
+        this.weight = DataInstance.defaultWeight;
+    }
 
-	@Override
-	public void setCursor(int cursor) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void setCursor(int cursor) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public int getCursor() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public int getCursor() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public int getQualityFeature(int index) {
-		return this.qualityFeatures[index];
-	}
+    @Override
+    public int getQualityFeature(int index) {
+        return this.qualityFeatures[index];
+    }
 
-	@Override
-	public float getQuantityFeature(int index) {
-		return this.quantityFeatures[index];
-	}
+    @Override
+    public float getQuantityFeature(int index) {
+        return this.quantityFeatures[index];
+    }
 
-	@Override
-	public ArrayInstance iterateQualityFeatures(QualityAccessor accessor) {
-		for (int index = 0; index < qualityOrder; index++) {
-			accessor.accessorFeature(index, this.qualityFeatures[index]);
-		}
-		return this;
-	}
+    @Override
+    public ArrayInstance iterateQualityFeatures(QualityAccessor accessor) {
+        for (int index = 0; index < qualityOrder; index++) {
+            accessor.accessorFeature(index, this.qualityFeatures[index]);
+        }
+        return this;
+    }
 
-	@Override
-	public ArrayInstance iterateQuantityFeatures(QuantityAccessor accessor) {
-		for (int index = 0; index < quantityOrder; index++) {
-			accessor.accessorFeature(index, this.quantityFeatures[index]);
-		}
-		return this;
-	}
+    @Override
+    public ArrayInstance iterateQuantityFeatures(QuantityAccessor accessor) {
+        for (int index = 0; index < quantityOrder; index++) {
+            accessor.accessorFeature(index, this.quantityFeatures[index]);
+        }
+        return this;
+    }
 
-	@Override
-	public int getQualityOrder() {
-		return qualityOrder;
-	}
+    @Override
+    public int getQualityOrder() {
+        return qualityOrder;
+    }
 
-	@Override
-	public int getQuantityOrder() {
-		return quantityOrder;
-	}
+    @Override
+    public int getQuantityOrder() {
+        return quantityOrder;
+    }
 
-	@Override
-	public int getQualityMark() {
-		return qualityMark;
-	}
+    @Override
+    public int getQualityMark() {
+        return qualityMark;
+    }
 
-	@Override
-	public float getQuantityMark() {
-		return quantityMark;
-	}
+    @Override
+    public float getQuantityMark() {
+        return quantityMark;
+    }
 
-	@Override
-	public float getWeight() {
-		return weight;
-	}
+    @Override
+    public float getWeight() {
+        return weight;
+    }
 
-	@Override
-	public void setQualityMark(int qualityMark) {
-		this.qualityMark = qualityMark;
-	}
+    @Override
+    public void setQualityMark(int qualityMark) {
+        this.qualityMark = qualityMark;
+    }
 
-	@Override
-	public void setQuantityMark(float quantityMark) {
-		this.quantityMark = quantityMark;
-	}
+    @Override
+    public void setQuantityMark(float quantityMark) {
+        this.quantityMark = quantityMark;
+    }
 
-	@Override
-	public void setWeight(float weight) {
-		this.weight = weight;
-	}
+    @Override
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
 
-	public void setQualityFeature(int index, int value) {
-		this.qualityFeatures[index] = value;
-	}
+    public void setQualityFeature(int index, int value) {
+        this.qualityFeatures[index] = value;
+    }
 
-	public void setQuantityFeature(int index, float value) {
-		this.quantityFeatures[index] = value;
-	}
+    public void setQuantityFeature(int index, float value) {
+        this.quantityFeatures[index] = value;
+    }
+
+    public void copyInstance(DataInstance instance) {
+        for (int index = 0; index < qualityOrder; index++) {
+            this.qualityFeatures[index] = DataInstance.defaultInteger;
+        }
+        for (int index = 0; index < quantityOrder; index++) {
+            this.quantityFeatures[index] = DataInstance.defaultFloat;
+        }
+        instance.iterateQualityFeatures((index, value) -> {
+            this.qualityFeatures[index] = value;
+        });
+        instance.iterateQuantityFeatures((index, value) -> {
+            this.quantityFeatures[index] = value;
+        });
+        this.qualityMark = instance.getQualityMark();
+        this.quantityMark = instance.getQuantityMark();
+        this.weight = instance.getWeight();
+    }
 
 }
--- src/main/java/com/jstarcraft/ai/data/module/HashInstance.java
+++ src/main/java/com/jstarcraft/ai/data/module/HashInstance.java
@@ -1,13 +1,14 @@
 package com.jstarcraft.ai.data.module;
 
-import com.jstarcraft.ai.data.QuantityAccessor;
 import com.jstarcraft.ai.data.DataInstance;
 import com.jstarcraft.ai.data.QualityAccessor;
-import com.jstarcraft.core.utility.ReflectionUtility;
+import com.jstarcraft.ai.data.QuantityAccessor;
 
 import it.unimi.dsi.fastutil.ints.Int2FloatMap;
+import it.unimi.dsi.fastutil.ints.Int2FloatRBTreeMap;
 import it.unimi.dsi.fastutil.ints.Int2FloatSortedMap;
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
+import it.unimi.dsi.fastutil.ints.Int2IntRBTreeMap;
 import it.unimi.dsi.fastutil.ints.Int2IntSortedMap;
 
 /**
@@ -18,127 +19,135 @@ import it.unimi.dsi.fastutil.ints.Int2IntSortedMap;
  */
 public class HashInstance implements DataInstance {
 
-	/** 离散秩 */
-	private int qualityOrder;
+    /** 离散秩 */
+    private int qualityOrder;
 
-	/** 连续秩 */
-	private int quantityOrder;
+    /** 连续秩 */
+    private int quantityOrder;
 
-	/** 离散特征 */
-	private Int2IntSortedMap qualityFeatures;
+    /** 离散特征 */
+    private Int2IntSortedMap qualityFeatures;
 
-	/** 连续特征 */
-	private Int2FloatSortedMap quantityFeatures;
+    /** 连续特征 */
+    private Int2FloatSortedMap quantityFeatures;
 
-	/** 离散标记 */
-	private int qualityMark;
+    /** 离散标记 */
+    private int qualityMark;
 
-	/** 连续标记 */
-	private float quantityMark;
+    /** 连续标记 */
+    private float quantityMark;
 
-	/** 权重 */
-	private float weight;
+    /** 权重 */
+    private float weight;
 
-	public HashInstance(Class<? extends Int2IntSortedMap> qualityClass, Class<? extends Int2FloatSortedMap> quantityClass, DataInstance instance) {
-		this.qualityOrder = instance.getQualityOrder();
-		this.quantityOrder = instance.getQuantityOrder();
-		this.qualityFeatures = ReflectionUtility.getInstance(qualityClass);
-		this.qualityFeatures.defaultReturnValue(DataInstance.defaultInteger);
-		this.quantityFeatures = ReflectionUtility.getInstance(quantityClass);
-		this.quantityFeatures.defaultReturnValue(DataInstance.defaultFloat);
-		instance.iterateQualityFeatures((index, value) -> {
-			this.qualityFeatures.put(index, value);
-		});
-		instance.iterateQuantityFeatures((index, value) -> {
-			this.quantityFeatures.put(index, value);
-		});
-		this.qualityMark = instance.getQualityMark();
-		this.quantityMark = instance.getQuantityMark();
-		this.weight = instance.getWeight();
-	}
+    public HashInstance(int qualityOrder, int quantityOrder) {
+        this.qualityOrder = qualityOrder;
+        this.quantityOrder = quantityOrder;
+        this.qualityFeatures = new Int2IntRBTreeMap();
+        this.qualityFeatures.defaultReturnValue(DataInstance.defaultInteger);
+        this.quantityFeatures = new Int2FloatRBTreeMap();
+        this.quantityFeatures.defaultReturnValue(DataInstance.defaultFloat);
+        this.qualityMark = defaultInteger;
+        this.quantityMark = defaultFloat;
+        this.weight = DataInstance.defaultWeight;
+    }
 
-	@Override
-	public void setCursor(int cursor) {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public void setCursor(int cursor) {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public int getCursor() {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public int getCursor() {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public int getQualityFeature(int index) {
-		return this.qualityFeatures.get(index);
-	}
+    @Override
+    public int getQualityFeature(int index) {
+        return this.qualityFeatures.get(index);
+    }
 
-	@Override
-	public float getQuantityFeature(int index) {
-		return this.quantityFeatures.get(index);
-	}
+    @Override
+    public float getQuantityFeature(int index) {
+        return this.quantityFeatures.get(index);
+    }
 
-	@Override
-	public HashInstance iterateQualityFeatures(QualityAccessor accessor) {
-		for (Int2IntMap.Entry term : this.qualityFeatures.int2IntEntrySet()) {
-			accessor.accessorFeature(term.getIntKey(), term.getIntValue());
-		}
-		return this;
-	}
+    @Override
+    public HashInstance iterateQualityFeatures(QualityAccessor accessor) {
+        for (Int2IntMap.Entry term : this.qualityFeatures.int2IntEntrySet()) {
+            accessor.accessorFeature(term.getIntKey(), term.getIntValue());
+        }
+        return this;
+    }
 
-	@Override
-	public HashInstance iterateQuantityFeatures(QuantityAccessor accessor) {
-		for (Int2FloatMap.Entry term : this.quantityFeatures.int2FloatEntrySet()) {
-			accessor.accessorFeature(term.getIntKey(), term.getFloatValue());
-		}
-		return this;
-	}
+    @Override
+    public HashInstance iterateQuantityFeatures(QuantityAccessor accessor) {
+        for (Int2FloatMap.Entry term : this.quantityFeatures.int2FloatEntrySet()) {
+            accessor.accessorFeature(term.getIntKey(), term.getFloatValue());
+        }
+        return this;
+    }
 
-	@Override
-	public int getQualityOrder() {
-		return qualityOrder;
-	}
+    @Override
+    public int getQualityOrder() {
+        return qualityOrder;
+    }
 
-	@Override
-	public int getQuantityOrder() {
-		return quantityOrder;
-	}
+    @Override
+    public int getQuantityOrder() {
+        return quantityOrder;
+    }
 
-	@Override
-	public int getQualityMark() {
-		return qualityMark;
-	}
+    @Override
+    public int getQualityMark() {
+        return qualityMark;
+    }
 
-	@Override
-	public float getQuantityMark() {
-		return quantityMark;
-	}
+    @Override
+    public float getQuantityMark() {
+        return quantityMark;
+    }
 
-	@Override
-	public float getWeight() {
-		return weight;
-	}
+    @Override
+    public float getWeight() {
+        return weight;
+    }
 
-	@Override
-	public void setQualityMark(int qualityMark) {
-		this.qualityMark = qualityMark;
-	}
+    @Override
+    public void setQualityMark(int qualityMark) {
+        this.qualityMark = qualityMark;
+    }
 
-	@Override
-	public void setQuantityMark(float quantityMark) {
-		this.quantityMark = quantityMark;
-	}
+    @Override
+    public void setQuantityMark(float quantityMark) {
+        this.quantityMark = quantityMark;
+    }
 
-	@Override
-	public void setWeight(float weight) {
-		this.weight = weight;
-	}
+    @Override
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
 
-	public void setQualityFeature(int index, int value) {
-		this.qualityFeatures.put(index, value);
-	}
+    public void setQualityFeature(int index, int value) {
+        this.qualityFeatures.put(index, value);
+    }
 
-	public void setquantityFeature(int index, float value) {
-		this.quantityFeatures.put(index, value);
-	}
+    public void setquantityFeature(int index, float value) {
+        this.quantityFeatures.put(index, value);
+    }
+
+    public void copyInstance(DataInstance instance) {
+        this.qualityFeatures.clear();
+        this.quantityFeatures.clear();
+        instance.iterateQualityFeatures((index, value) -> {
+            this.qualityFeatures.put(index, value);
+        });
+        instance.iterateQuantityFeatures((index, value) -> {
+            this.quantityFeatures.put(index, value);
+        });
+        this.qualityMark = instance.getQualityMark();
+        this.quantityMark = instance.getQuantityMark();
+        this.weight = instance.getWeight();
+    }
 
 }
