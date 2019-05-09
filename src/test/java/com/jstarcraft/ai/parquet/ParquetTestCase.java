37e5b2b95334860ccc07685277e7377094718c65
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-05-09 20:26:56 +0800

    Parquet单元测试.

--- src/test/java/com/jstarcraft/ai/parquet/ParquetTestCase.java
+++ src/test/java/com/jstarcraft/ai/parquet/ParquetTestCase.java
@@ -1,10 +1,16 @@
 package com.jstarcraft.ai.parquet;
 
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.util.Utf8;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RawLocalFileSystem;
-import org.apache.parquet.column.ParquetProperties;
+import org.apache.parquet.avro.AvroParquetReader;
+import org.apache.parquet.avro.AvroParquetWriter;
 import org.apache.parquet.example.data.Group;
 import org.apache.parquet.example.data.GroupFactory;
 import org.apache.parquet.example.data.simple.SimpleGroupFactory;
@@ -12,6 +18,8 @@ import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.example.GroupReadSupport;
 import org.apache.parquet.hadoop.example.GroupWriteSupport;
+import org.apache.parquet.hadoop.util.HadoopInputFile;
+import org.apache.parquet.hadoop.util.HadoopOutputFile;
 import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.MessageTypeParser;
 import org.junit.Assert;
@@ -92,6 +100,41 @@ public class ParquetTestCase {
             Assert.assertEquals("2020-01-01 00:00:00", group.getString("when", 0));
             Assert.assertEquals(0F, where.getFloat("longitude", 0), 0F);
             Assert.assertEquals(0F, where.getFloat("latitude", 0), 0F);
+            reader.close();
+        }
+    }
+
+    @Test
+    public void testReadWriteArvo() throws Exception {
+        fileSystem.delete(path, true);
+
+        Schema schema = SchemaBuilder.record("parquet").fields().requiredString("title").requiredString("when").name("where").type(SchemaBuilder.record("where").fields().requiredFloat("longitude").requiredFloat("latitude").endRecord()).noDefault().endRecord();
+
+        {
+            GenericRecord parquet = new GenericData.Record(schema);
+            parquet.put("title", "title");
+            parquet.put("when", "2020-01-01 00:00:00");
+            GenericRecord where = new GenericData.Record(schema.getField("where").schema());
+            where.put("longitude", 0F);
+            where.put("latitude", 0F);
+            parquet.put("where", where);
+
+            HadoopOutputFile output = HadoopOutputFile.fromPath(path, configuration);
+            ParquetWriter writer = AvroParquetWriter.builder(output).withSchema(schema).build();
+            writer.write(parquet);
+            writer.close();
+        }
+
+        {
+            HadoopInputFile input = HadoopInputFile.fromPath(path, configuration);
+            ParquetReader reader = AvroParquetReader.builder(input).build();
+            GenericRecord parquet = (GenericRecord) reader.read();
+            GenericRecord where = (GenericRecord) parquet.get("where");
+
+            Assert.assertEquals("title", Utf8.class.cast(parquet.get("title")).toString());
+            Assert.assertEquals("2020-01-01 00:00:00", Utf8.class.cast(parquet.get("when")).toString());
+            Assert.assertEquals(0F, where.get("longitude"));
+            Assert.assertEquals(0F, where.get("latitude"));
             reader.close();
         }
     }
