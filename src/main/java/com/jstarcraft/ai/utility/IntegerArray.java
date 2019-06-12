aa300dab5cbe173580ebf5ded91f6812784d1a96
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-06-12 17:33:00 +0800

    调整FloatArray与IntegerArray构造器.

--- src/main/java/com/jstarcraft/ai/utility/IntegerArray.java
+++ src/main/java/com/jstarcraft/ai/utility/IntegerArray.java
@@ -10,66 +10,65 @@ import java.util.ArrayList;
  */
 public class IntegerArray implements CapacityArray {
 
-	/** 最大容量 */
-	private int maximumCapacity;
+    /** 最大容量 */
+    private int maximumCapacity;
 
-	/** 最小容量 */
-	private int minimumCapacity;
+    /** 最小容量 */
+    private int minimumCapacity;
 
-	private int[] current;
+    private int[] current;
 
-	private ArrayList<int[]> datas;
+    private ArrayList<int[]> datas;
 
-	/** 大小 */
-	private int size;
+    /** 大小 */
+    private int size;
 
-	@Deprecated
-	public IntegerArray() {
-		this(10000, 10000 * 10000);
-	}
+    public IntegerArray() {
+        this(1000, 1000 * 1000 * 1000);
+    }
 
-	public IntegerArray(int minimumCapacity, int maximumCapacity) {
-		this.minimumCapacity = minimumCapacity;
-		this.maximumCapacity = maximumCapacity;
-		this.datas = new ArrayList<>(maximumCapacity / minimumCapacity + (maximumCapacity % minimumCapacity == 0 ? 0 : 1));
-		this.size = 0;
-	}
+    public IntegerArray(int minimumCapacity, int maximumCapacity) {
+        this.minimumCapacity = minimumCapacity;
+        this.maximumCapacity = maximumCapacity;
+        this.datas = new ArrayList<>(maximumCapacity / minimumCapacity + (maximumCapacity % minimumCapacity == 0 ? 0 : 1));
+        this.size = 0;
+    }
 
-	public void associateData(int data) {
-		int position = size++ % minimumCapacity;
-		if (position == 0) {
-			if (size >= maximumCapacity) {
-				current = null;
-				throw new IllegalStateException();
-			} else {
-				current = new int[minimumCapacity];
-				datas.add(current);
-			}
-		}
-		current[position] = data;
-	}
+    public void associateData(int data) {
+        int position = size++ % minimumCapacity;
+        if (position == 0) {
+            if (size >= maximumCapacity) {
+                current = null;
+                throw new IllegalStateException();
+            } else {
+                current = new int[minimumCapacity];
+                datas.add(current);
+            }
+        }
+        current[position] = data;
+    }
 
-	public int getData(int cursor) {
-		return datas.get(cursor / minimumCapacity)[cursor % minimumCapacity];
-	}
+    public int getData(int cursor) {
+        return datas.get(cursor / minimumCapacity)[cursor % minimumCapacity];
+    }
 
-	public void setData(int cursor, int data) {
-		datas.get(cursor / minimumCapacity)[cursor % minimumCapacity] = data;
-	}
+    public void setData(int cursor, int data) {
+        datas.get(cursor / minimumCapacity)[cursor % minimumCapacity] = data;
+    }
 
-	@Override
-	public int getMaximumCapacity() {
-		return maximumCapacity;
-	}
+    @Override
+    public int getMaximumCapacity() {
+        return maximumCapacity;
+    }
 
-	@Override
-	public int getMinimumCapacity() {
-		return minimumCapacity;
-	}
+    @Override
+    public int getMinimumCapacity() {
+        return minimumCapacity;
+    }
 
-	@Override
-	public int getSize() {
-		return size;
-	}
+    @Override
+    public int getSize() {
+        return size;
+    }
 
 }
