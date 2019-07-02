5ef20f7a0037c6d50464e5edf91da1b58a7d5070
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-07-02 18:32:07 +0800

    Lucene单元测试重构.

--- src/test/java/com/jstarcraft/ai/retrieval/LuceneTestCase.java
+++ src/test/java/com/jstarcraft/ai/retrieval/LuceneTestCase.java
@@ -1,9 +1,5 @@
 package com.jstarcraft.ai.retrieval;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Document;
@@ -19,8 +15,8 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -31,47 +27,42 @@ public class LuceneTestCase {
 
     @Test
     public void test() throws Exception {
-        // 索引路径
-        Path indexPath = Paths.get("lucene", "indexes");
-        FileUtils.forceDelete(indexPath.toFile());
-        Directory directory = FSDirectory.open(indexPath);
-
+        Directory directory = new ByteBuffersDirectory();
         Analyzer analyzer = new HanlpIndexAnalyzer();
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         IndexWriter indexWriter = new IndexWriter(directory, config);
-
-        // 文档路径
-        Path documentPath = Paths.get("lucene", "documents");
-        for (File file : documentPath.toFile().listFiles()) {
+        {
+            String fileContent = "我爱你";
             Document document = new Document();
-
             // 文件名称
-            String fileName = file.getName();
-            Field fileNameField = new TextField("fileName", fileName, Store.YES);
+            Field fileNameField = new TextField("fileName", "chinese.txt", Store.YES);
             document.add(fileNameField);
-
             // 文件大小
-            long fileSize = FileUtils.sizeOf(file);
-            Field fileSizeField = new StoredField("fileSize", fileSize);
+            Field fileSizeField = new StoredField("fileSize", fileContent.getBytes(StringUtility.CHARSET).length);
             document.add(fileSizeField);
-
-            // 文件路径
-            String filePath = file.getPath();
-            Field filePathField = new StoredField("filePath", filePath);
-            document.add(filePathField);
-
             // 文件内容
-            String fileContent = FileUtils.readFileToString(file, StringUtility.CHARSET);
             Field fileContentField = new TextField("fileContent", fileContent, Store.YES);
             document.add(fileContentField);
-
+            indexWriter.addDocument(document);
+        }
+        {
+            String fileContent = "i love you";
+            Document document = new Document();
+            // 文件名称
+            Field fileNameField = new TextField("fileName", "english.txt", Store.YES);
+            document.add(fileNameField);
+            // 文件大小
+            Field fileSizeField = new StoredField("fileSize", fileContent.getBytes(StringUtility.CHARSET).length);
+            document.add(fileSizeField);
+            // 文件内容
+            Field fileContentField = new TextField("fileContent", fileContent, Store.YES);
+            document.add(fileContentField);
             indexWriter.addDocument(document);
         }
         indexWriter.close();
 
         IndexReader indexReader = DirectoryReader.open(directory);
         IndexSearcher indexSearcher = new IndexSearcher(indexReader);
-
         Query query = new MatchAllDocsQuery();
         TopDocs topDocs = indexSearcher.search(query, 10);
         Assert.assertEquals(2L, topDocs.totalHits.value);
@@ -83,8 +74,6 @@ public class LuceneTestCase {
             Assert.assertEquals("我爱你", document.get("fileContent"));
             // 文件大小
             Assert.assertEquals("9", document.get("fileSize"));
-            // 文件路径
-            Assert.assertEquals("lucene\\documents\\chinese.txt", document.get("filePath"));
         }
         {
             Document document = indexSearcher.doc(topDocs.scoreDocs[1].doc);
@@ -94,8 +83,6 @@ public class LuceneTestCase {
             Assert.assertEquals("i love you", document.get("fileContent"));
             // 文件大小
             Assert.assertEquals("10", document.get("fileSize"));
-            // 文件路径
-            Assert.assertEquals("lucene\\documents\\english.txt", document.get("filePath"));
         }
         indexReader.close();
     }
