b843a75cf95dd68489eeb004460b6cabafa0a2f7
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-08-25 12:34:41 +0800

    格式化代码.

--- jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/correlation/MSDSimilarity.java
+++ jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/correlation/MSDSimilarity.java
@@ -13,39 +13,39 @@ import com.jstarcraft.core.utility.Float2FloatKeyValue;
  */
 public class MSDSimilarity extends AbstractSimilarity {
 
-	private float getSimilarity(int count, List<Float2FloatKeyValue> scoreList) {
-		if (count == 0) {
-			return Float.NaN;
-		}
-		float sum = 0F;
-		for (Float2FloatKeyValue term : scoreList) {
-			sum += Math.pow(term.getKey() - term.getValue(), 2);
-		}
-		float similarity = count / sum;
-		if (Float.isInfinite(similarity)) {
-			similarity = 0F;
-		}
-		return similarity;
-	}
+    private float getSimilarity(int count, List<Float2FloatKeyValue> scoreList) {
+        if (count == 0) {
+            return Float.NaN;
+        }
+        float sum = 0F;
+        for (Float2FloatKeyValue term : scoreList) {
+            sum += Math.pow(term.getKey() - term.getValue(), 2);
+        }
+        float similarity = count / sum;
+        if (Float.isInfinite(similarity)) {
+            similarity = 0F;
+        }
+        return similarity;
+    }
 
-	@Override
-	public float getCoefficient(MathVector leftVector, MathVector rightVector, float scale) {
-		// compute similarity
-		List<Float2FloatKeyValue> scoreList = getScoreList(leftVector, rightVector);
-		int count = scoreList.size();
-		float similarity = getSimilarity(count, scoreList);
-		// shrink to account for vector size
-		if (!Double.isNaN(similarity)) {
-			if (scale > 0) {
-				similarity *= count / (count + scale);
-			}
-		}
-		return similarity;
-	}
+    @Override
+    public float getCoefficient(MathVector leftVector, MathVector rightVector, float scale) {
+        // compute similarity
+        List<Float2FloatKeyValue> scoreList = getScoreList(leftVector, rightVector);
+        int count = scoreList.size();
+        float similarity = getSimilarity(count, scoreList);
+        // shrink to account for vector size
+        if (!Double.isNaN(similarity)) {
+            if (scale > 0) {
+                similarity *= count / (count + scale);
+            }
+        }
+        return similarity;
+    }
 
-	@Override
-	public float getIdentical() {
-		return 0F;
-	}
+    @Override
+    public float getIdentical() {
+        return 0F;
+    }
 
 }
--- jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/correlation/MSESimilarity.java
+++ jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/correlation/MSESimilarity.java
@@ -13,36 +13,36 @@ import com.jstarcraft.core.utility.Float2FloatKeyValue;
  */
 public class MSESimilarity extends AbstractSimilarity {
 
-	private float getSimilarity(int count, List<Float2FloatKeyValue> scoreList) {
-		if (count == 0) {
-			return Float.NaN;
-		}
-		float similarity = 0F;
-		for (Float2FloatKeyValue term : scoreList) {
-			float delta = term.getKey() - term.getValue();
-			similarity += Math.pow(delta, 2);
-		}
-		return similarity / count;
-	}
+    private float getSimilarity(int count, List<Float2FloatKeyValue> scoreList) {
+        if (count == 0) {
+            return Float.NaN;
+        }
+        float similarity = 0F;
+        for (Float2FloatKeyValue term : scoreList) {
+            float delta = term.getKey() - term.getValue();
+            similarity += Math.pow(delta, 2);
+        }
+        return similarity / count;
+    }
 
-	@Override
-	public float getCoefficient(MathVector leftVector, MathVector rightVector, float scale) {
-		// compute similarity
-		List<Float2FloatKeyValue> scoreList = getScoreList(leftVector, rightVector);
-		int count = scoreList.size();
-		float similarity = getSimilarity(count, scoreList);
-		// shrink to account for vector size
-		if (!Double.isNaN(similarity)) {
-			if (scale > 0) {
-				similarity *= count / (count + scale);
-			}
-		}
-		return similarity;
-	}
+    @Override
+    public float getCoefficient(MathVector leftVector, MathVector rightVector, float scale) {
+        // compute similarity
+        List<Float2FloatKeyValue> scoreList = getScoreList(leftVector, rightVector);
+        int count = scoreList.size();
+        float similarity = getSimilarity(count, scoreList);
+        // shrink to account for vector size
+        if (!Double.isNaN(similarity)) {
+            if (scale > 0) {
+                similarity *= count / (count + scale);
+            }
+        }
+        return similarity;
+    }
 
-	@Override
-	public float getIdentical() {
-		return 0F;
-	}
+    @Override
+    public float getIdentical() {
+        return 0F;
+    }
 
 }
