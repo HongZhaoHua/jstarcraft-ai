08d04557b8b360e1863e5d4487fbbab5ed31c358
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-09-18 20:11:05 +0800

    重构代码注释.

--- jstarcraft-ai-lsh/src/test/java/be/tarsos/lsh/VectorTests.java
+++ jstarcraft-ai-lsh/src/test/java/be/tarsos/lsh/VectorTests.java
@@ -1,30 +1,8 @@
-/*
-*      _______                       _        ____ _     _
-*     |__   __|                     | |     / ____| |   | |
-*        | | __ _ _ __ ___  ___  ___| |    | (___ | |___| |
-*        | |/ _` | '__/ __|/ _ \/ __| |     \___ \|  ___  |
-*        | | (_| | |  \__ \ (_) \__ \ |____ ____) | |   | |
-*        |_|\__,_|_|  |___/\___/|___/_____/|_____/|_|   |_|
-*                                                         
-* -----------------------------------------------------------
-*
-*  TarsosLSH is developed by Joren Six.
-*  
-* -----------------------------------------------------------
-*
-*  Info    : http://0110.be/tag/TarsosLSH
-*  Github  : https://github.com/JorenSix/TarsosLSH
-*  Releases: http://0110.be/releases/TarsosLSH/
-* 
-*/
-
 package be.tarsos.lsh;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
-import be.tarsos.lsh.Vector;
 
 public class VectorTests {
 
--- jstarcraft-ai-lsh/src/test/java/be/tarsos/lsh/families/CityBlockHashTestCase.java
+++ jstarcraft-ai-lsh/src/test/java/be/tarsos/lsh/families/CityBlockHashTestCase.java
@@ -1,23 +1,3 @@
-/*
-*      _______                       _        ____ _     _
-*     |__   __|                     | |     / ____| |   | |
-*        | | __ _ _ __ ___  ___  ___| |    | (___ | |___| |
-*        | |/ _` | '__/ __|/ _ \/ __| |     \___ \|  ___  |
-*        | | (_| | |  \__ \ (_) \__ \ |____ ____) | |   | |
-*        |_|\__,_|_|  |___/\___/|___/_____/|_____/|_|   |_|
-*                                                         
-* -----------------------------------------------------------
-*
-*  TarsosLSH is developed by Joren Six.
-*  
-* -----------------------------------------------------------
-*
-*  Info    : http://0110.be/tag/TarsosLSH
-*  Github  : https://github.com/JorenSix/TarsosLSH
-*  Releases: http://0110.be/releases/TarsosLSH/
-* 
-*/
-
 package be.tarsos.lsh.families;
 
 import static org.junit.Assert.assertEquals;
