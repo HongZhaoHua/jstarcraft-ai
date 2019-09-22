289a2e906a62c348fbb81e3fc4f9dcd5e312cc4e
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-09-22 09:36:37 +0800

    注释系数.

--- jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/correlation/Coefficient.java
+++ jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/correlation/Coefficient.java
@@ -2,10 +2,27 @@ package com.jstarcraft.ai.math.algorithm.correlation;
 
 import com.jstarcraft.ai.math.structure.vector.VectorScalar;
 
+/**
+ * 系数
+ * 
+ * @author Birdy
+ *
+ */
 public interface Coefficient {
 
+    /**
+     * 计算系数
+     * 
+     * @param leftScalar
+     * @param rightScalar
+     */
     void calculateScore(VectorScalar leftScalar, VectorScalar rightScalar);
-    
+
+    /**
+     * 获取系数
+     * 
+     * @return
+     */
     float getScore();
-    
+
 }
