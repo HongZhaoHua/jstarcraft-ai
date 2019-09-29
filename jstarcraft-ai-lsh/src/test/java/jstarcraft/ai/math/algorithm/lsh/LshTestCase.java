abf378cfcfb9f1516d95fa6c74c0072d573dfca0
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-09-29 18:46:45 +0800

    重构LSH单元测试.

--- jstarcraft-ai-lsh/src/test/java/jstarcraft/ai/math/algorithm/lsh/LshTestCase.java
+++ jstarcraft-ai-lsh/src/test/java/jstarcraft/ai/math/algorithm/lsh/LshTestCase.java
@@ -1,99 +1,129 @@
 package jstarcraft.ai.math.algorithm.lsh;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.jstarcraft.ai.math.structure.vector.MathVector;
+import com.jstarcraft.core.utility.StringUtility;
 
 import be.tarsos.lsh.KeyVector;
 
 public class LshTestCase {
 
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private List<MathVector> dataset = new ArrayList<>(4);
+
+    private MathVector left = new KeyVector("left", new float[] { 1F, 1F, 0F, 0F, 0F, 1F, 1F });
+
+    private MathVector middle = new KeyVector("middle", new float[] { 0F, 0F, 1F, 1F, 1F, 0F, 0F });
+
+    private MathVector right = new KeyVector("right", new float[] { 1F, 0F, 0F, 0F, 0F, 1F, 1F });
+
+    private MathVector query = new KeyVector("query", new float[] { 0F, 1F, 1F, 1F, 1F, 0F, 0F });
+
     {
-        MathVector left = new KeyVector("left", new float[] { 1F, 1F, 0F, 0F, 0F, 1F, 1F });
         dataset.add(left);
-        MathVector middle = new KeyVector("middle", new float[] { 0F, 0F, 1F, 1F, 1F, 0F, 0F });
         dataset.add(middle);
-        MathVector right = new KeyVector("right", new float[] { 1F, 0F, 0F, 0F, 0F, 1F, 1F });
         dataset.add(right);
-        MathVector query = new KeyVector("query", new float[] { 0F, 1F, 1F, 1F, 1F, 0F, 0F });
         dataset.add(query);
+    }
+
+    private int[] signature(MathVector data, HashFunction[] hashs) {
+        int size = hashs.length;
+        int[] signature = new int[size];
+        for (int index = 0; index < size; index++) {
+            HashFunction hash = hashs[index];
+            signature[index] = hash.hash(data);
+        }
+        return signature;
     }
 
     @Test
     public void testCosineHash() {
-        Random rand = new Random(0);
-        List<HashFunction> hashs = new ArrayList<>(4);
-        for (int index = 0; index < 4; index++) {
-            CosineHash hash = new CosineHash(rand, 7);
-            hashs.add(hash);
+        Random random = new Random(0);
+        HashFunction[] hashs = new HashFunction[3];
+        for (int index = 0; index < 3; index++) {
+            CosineHash hash = new CosineHash(random, 7);
+            hashs[index] = hash;
         }
-        for (MathVector data : dataset) {
-            StringBuilder buffer = new StringBuilder("CosineHash:");
-            for (HashFunction hash : hashs) {
-                int hashValue = hash.hash(data);
-                buffer.append(hashValue).append(",");
+
+        if (logger.isDebugEnabled()) {
+            for (MathVector data : dataset) {
+                String message = StringUtility.format("CosineHash:{}", Arrays.toString(signature(data, hashs)));
+                logger.debug(message);
             }
-            System.out.println(buffer.toString());
         }
+
+        Assert.assertArrayEquals(signature(left, hashs), signature(right, hashs));
+        Assert.assertArrayEquals(signature(middle, hashs), signature(query, hashs));
     }
 
     @Test
     public void testEuclideanHash() {
-        Random rand = new Random(0);
-        List<HashFunction> hashs = new ArrayList<>(4);
-        for (int index = 0; index < 4; index++) {
-            EuclideanHash hash = new EuclideanHash(rand, 7, 7);
-            hashs.add(hash);
+        Random random = new Random(0);
+        HashFunction[] hashs = new HashFunction[3];
+        for (int index = 0; index < 3; index++) {
+            EuclideanHash hash = new EuclideanHash(random, 7, 7);
+            hashs[index] = hash;
         }
-        for (MathVector data : dataset) {
-            StringBuilder buffer = new StringBuilder("EuclideanHash:");
-            for (HashFunction hash : hashs) {
-                int hashValue = hash.hash(data);
-                buffer.append(hashValue).append(",");
+
+        if (logger.isDebugEnabled()) {
+            for (MathVector data : dataset) {
+                String message = StringUtility.format("EuclideanHash:{}", Arrays.toString(signature(data, hashs)));
+                logger.debug(message);
             }
-            System.out.println(buffer.toString());
         }
+
+        Assert.assertArrayEquals(signature(left, hashs), signature(right, hashs));
+        Assert.assertArrayEquals(signature(middle, hashs), signature(query, hashs));
     }
 
     @Test
     public void testManhattanHash() {
-        Random rand = new Random(0);
-        List<HashFunction> hashs = new ArrayList<>(4);
-        for (int index = 0; index < 4; index++) {
-            ManhattanHash hash = new ManhattanHash(rand, 7, 7);
-            hashs.add(hash);
+        Random random = new Random(0);
+        HashFunction[] hashs = new HashFunction[3];
+        for (int index = 0; index < 3; index++) {
+            ManhattanHash hash = new ManhattanHash(random, 7, 7);
+            hashs[index] = hash;
         }
-        for (MathVector data : dataset) {
-            StringBuilder buffer = new StringBuilder("ManhattanHash:");
-            for (HashFunction hash : hashs) {
-                int hashValue = hash.hash(data);
-                buffer.append(hashValue).append(",");
+
+        if (logger.isDebugEnabled()) {
+            for (MathVector data : dataset) {
+                String message = StringUtility.format("ManhattanHash:{}", Arrays.toString(signature(data, hashs)));
+                logger.debug(message);
             }
-            System.out.println(buffer.toString());
         }
+
+        Assert.assertArrayEquals(signature(left, hashs), signature(right, hashs));
+        Assert.assertArrayEquals(signature(middle, hashs), signature(query, hashs));
     }
 
     @Test
     public void testMinHash() {
-        Random rand = new Random(0);
-        List<HashFunction> hashs = new ArrayList<>(4);
-        for (int index = 0; index < 4; index++) {
-            MinHash hash = new MinHash(rand);
-            hashs.add(hash);
+        Random random = new Random(0);
+        HashFunction[] hashs = new HashFunction[3];
+        for (int index = 0; index < 3; index++) {
+            MinHash hash = new MinHash(random);
+            hashs[index] = hash;
         }
-        for (MathVector data : dataset) {
-            StringBuilder buffer = new StringBuilder("MinHash:");
-            for (HashFunction hash : hashs) {
-                int hashValue = hash.hash(data);
-                buffer.append(hashValue).append(",");
+
+        if (logger.isDebugEnabled()) {
+            for (MathVector data : dataset) {
+                String message = StringUtility.format("MinHash:{}", Arrays.toString(signature(data, hashs)));
+                logger.debug(message);
             }
-            System.out.println(buffer.toString());
         }
+
+        Assert.assertArrayEquals(signature(left, hashs), signature(right, hashs));
+        Assert.assertArrayEquals(signature(middle, hashs), signature(query, hashs));
     }
 
 }
