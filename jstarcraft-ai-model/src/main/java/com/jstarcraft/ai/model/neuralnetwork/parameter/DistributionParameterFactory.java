a82b153164f4d4c4375ff097b46cbe3b40fad4a7
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-10-19 18:43:26 +0800

    重构相关性,距离与相似度.

--- jstarcraft-ai-model/src/main/java/com/jstarcraft/ai/model/neuralnetwork/parameter/DistributionParameterFactory.java
+++ jstarcraft-ai-model/src/main/java/com/jstarcraft/ai/model/neuralnetwork/parameter/DistributionParameterFactory.java
@@ -1,16 +1,16 @@
 package com.jstarcraft.ai.model.neuralnetwork.parameter;
 
-import com.jstarcraft.ai.math.algorithm.probability.Probability;
+import com.jstarcraft.ai.math.algorithm.probability.MathProbability;
 import com.jstarcraft.ai.math.structure.matrix.MathMatrix;
 
 public class DistributionParameterFactory implements ParameterFactory {
 
-    private Probability<Number> probability;
+    private MathProbability<Number> probability;
 
     DistributionParameterFactory() {
     }
 
-    public DistributionParameterFactory(Probability<Number> probability) {
+    public DistributionParameterFactory(MathProbability<Number> probability) {
         this.probability = probability;
     }
 
