78fd76adca66c0d3ebca60bc7728dea6eef6a0f9
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-10-24 11:23:15 +0800

    清理多余文件.

--- jstarcraft-ai-lsh/src/main/resources/orestes/bloomfilter/cachesketch/reportRead.lua
+++
@@ -1,6 +0,0 @@
-local newScore = tonumber(ARGV[1])
-local element = ARGV[2]
-local score = redis.call('zscore', KEYS[1], element)
-if score == false or newScore > tonumber(score) then
-    redis.call('zadd', KEYS[1], newScore, element)
-end
