965ee773fc182ccc760fcb3382dba48e4adf15e5
Birdy <Birdy@LAPTOP-QRG8T75T>
2019-12-12 15:06:57 +0800

    朴素贝叶斯分类器.

---
+++ jstarcraft-ai-model/src/main/java/com/jstarcraft/ai/model/pg/supervised/classify/NaiveBayesianClassifier.java
@@ -0,0 +1,28 @@
+package com.jstarcraft.ai.model.pg.supervised.classify;
+
+import com.jstarcraft.ai.data.DataInstance;
+import com.jstarcraft.ai.data.DataModule;
+import com.jstarcraft.ai.model.supervised.Practicer;
+import com.jstarcraft.ai.model.supervised.Predictor;
+
+/**
+ * 朴素贝叶斯分类器
+ * 
+ * @author Birdy
+ *
+ */
+public class NaiveBayesianClassifier implements Practicer, Predictor {
+
+    @Override
+    public void practice(DataModule module, DataModule... contexts) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void predict(DataInstance instance, DataInstance... contexts) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
