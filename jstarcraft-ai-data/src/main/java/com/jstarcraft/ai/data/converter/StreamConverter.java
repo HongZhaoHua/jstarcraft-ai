17304ca53ccf52a4663b51e817a0f564959453f7
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-05-12 10:21:22 +0800

    InputStreamReader支持指定字符集.

--- jstarcraft-ai-data/src/main/java/com/jstarcraft/ai/data/converter/StreamConverter.java
+++ jstarcraft-ai-data/src/main/java/com/jstarcraft/ai/data/converter/StreamConverter.java
@@ -10,6 +10,7 @@ import com.jstarcraft.ai.data.DataModule;
 import com.jstarcraft.ai.data.attribute.QualityAttribute;
 import com.jstarcraft.ai.data.attribute.QuantityAttribute;
 import com.jstarcraft.ai.data.exception.DataException;
+import com.jstarcraft.core.utility.StringUtility;
 
 /**
  * 流式转换器
@@ -28,7 +29,7 @@ public abstract class StreamConverter extends AbstractConverter<InputStream> {
     @Override
     public int convert(DataModule module, InputStream stream) {
         try {
-            try (InputStreamReader iterator = new InputStreamReader(stream); BufferedReader buffer = new BufferedReader(iterator)) {
+            try (InputStreamReader iterator = new InputStreamReader(stream, StringUtility.CHARSET); BufferedReader buffer = new BufferedReader(iterator)) {
                 return parseData(module, buffer);
             }
         } catch (Exception exception) {
