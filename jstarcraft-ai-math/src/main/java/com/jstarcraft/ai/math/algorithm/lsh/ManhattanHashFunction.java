d78da8b16cf9eda5111f7aa1b67fbfe75115515d
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-05-14 20:09:04 +0800

    格式化代码.

--- jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/lsh/ManhattanHashFunction.java
+++ jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/lsh/ManhattanHashFunction.java
@@ -8,31 +8,29 @@ import com.jstarcraft.ai.math.structure.vector.MathVector;
 
 public class ManhattanHashFunction implements VectorHashFunction {
 
-    private int w;
+	private int w;
 
-    private MathVector projection;
+	private MathVector projection;
 
-    public ManhattanHashFunction(Random random, int dimensions, int width) {
-        this.w = width;
+	public ManhattanHashFunction(Random random, int dimensions, int width) {
+		this.w = width;
 
-        projection = new ArrayVector(dimensions, new float[dimensions]);
-        for (int dimension = 0; dimension < dimensions; dimension++) {
-            // mean 0
-            // standard deviation 1.0
-            float val = random.nextFloat() * w;
-            projection.setValue(dimension, val);
-        }
-    }
+		projection = new ArrayVector(dimensions, new float[dimensions]);
+		for (int dimension = 0; dimension < dimensions; dimension++) {
+			// mean 0
+			// standard deviation 1.0
+			float val = random.nextFloat() * w;
+			projection.setValue(dimension, val);
+		}
+	}
 
-    public int hash(MathVector vector) {
-        int hash[] = new int[projection.getDimensionSize()];
-        for (int dimension = 0; dimension < projection.getDimensionSize(); dimension++) {
-            hash[dimension] = (int) Math.floor((vector.getValue(dimension) - projection.getValue(dimension)) / Float.valueOf(w));
-        }
-        return Arrays.hashCode(hash);
-    }
+	@Override
+	public int hash(MathVector vector) {
+		int hash[] = new int[projection.getDimensionSize()];
+		for (int dimension = 0; dimension < projection.getDimensionSize(); dimension++) {
+			hash[dimension] = (int) Math.floor((vector.getValue(dimension) - projection.getValue(dimension)) / Float.valueOf(w));
+		}
+		return Arrays.hashCode(hash);
+	}
 
-    public String toString() {
-        return String.format("w:%d\nrandomPartition:%s", w, projection);
-    }
 }
