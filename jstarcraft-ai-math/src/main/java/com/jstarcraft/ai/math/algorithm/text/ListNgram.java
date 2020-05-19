f1529f591785d38f1a7728d3a51438c077b435d4
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-05-19 20:03:23 +0800

    注释N元模型.

--- jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/text/ListNgram.java
+++ jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/text/ListNgram.java
@@ -6,57 +6,64 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * 基于列表的N元模型
+ * 
+ * @author Birdy
+ *
+ * @param <T>
+ */
 public class ListNgram<T> implements Ngram<List<T>> {
 
-    private int grams;
+	private int grams;
 
-    private List<T> terms;
+	private List<T> terms;
 
-    public ListNgram(int grams, T... terms) {
-        assert grams <= terms.length;
-        this.grams = grams;
-        this.terms = Arrays.asList(terms);
-    }
+	public ListNgram(int grams, T... terms) {
+		assert grams <= terms.length;
+		this.grams = grams;
+		this.terms = Arrays.asList(terms);
+	}
 
-    public ListNgram(int grams, Collection<T> terms) {
-        assert grams <= terms.size();
-        this.grams = grams;
-        this.terms = new ArrayList<>(terms);
-    }
+	public ListNgram(int grams, Collection<T> terms) {
+		assert grams <= terms.size();
+		this.grams = grams;
+		this.terms = new ArrayList<>(terms);
+	}
 
-    @Override
-    public int getSize() {
-        return terms.size() - grams + 1;
-    }
+	@Override
+	public int getSize() {
+		return terms.size() - grams + 1;
+	}
 
-    @Override
-    public Iterator<List<T>> iterator() {
-        return new NgramIterator();
-    }
+	@Override
+	public Iterator<List<T>> iterator() {
+		return new NgramIterator();
+	}
 
-    private class NgramIterator implements Iterator<List<T>> {
+	private class NgramIterator implements Iterator<List<T>> {
 
-        private int size = terms.size();
+		private int size = terms.size();
 
-        private int cursor;
+		private int cursor;
 
-        @Override
-        public boolean hasNext() {
-            return cursor + grams <= size;
-        }
+		@Override
+		public boolean hasNext() {
+			return cursor + grams <= size;
+		}
 
-        @Override
-        public List<T> next() {
-            List<T> ngram = terms.subList(cursor, cursor + grams);
-            cursor++;
-            return ngram;
-        }
+		@Override
+		public List<T> next() {
+			List<T> ngram = terms.subList(cursor, cursor + grams);
+			cursor++;
+			return ngram;
+		}
 
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
 
-    }
+	}
 
 }
--- jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/text/Ngram.java
+++ jstarcraft-ai-math/src/main/java/com/jstarcraft/ai/math/algorithm/text/Ngram.java
@@ -1,7 +1,14 @@
 package com.jstarcraft.ai.math.algorithm.text;
 
+/**
+ * N元模型
+ * 
+ * @author Birdy
+ *
+ * @param <N>
+ */
 public interface Ngram<N> extends Iterable<N> {
 
-    int getSize();
+	int getSize();
 
 }
