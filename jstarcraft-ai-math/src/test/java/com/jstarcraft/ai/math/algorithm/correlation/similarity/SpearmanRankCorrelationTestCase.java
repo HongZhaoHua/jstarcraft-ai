1940bc0c783ee36f7a163e18e8cb8854cebda276
Birdy <Birdy@LAPTOP-QRG8T75T>
2020-06-08 18:25:29 +0800

    重构单元测试.

--- jstarcraft-ai-math/src/test/java/com/jstarcraft/ai/math/algorithm/correlation/similarity/SpearmanRankCorrelationTestCase.java
+++ jstarcraft-ai-math/src/test/java/com/jstarcraft/ai/math/algorithm/correlation/similarity/SpearmanRankCorrelationTestCase.java
@@ -2,7 +2,6 @@ package com.jstarcraft.ai.math.algorithm.correlation.similarity;
 
 import com.jstarcraft.ai.math.algorithm.correlation.AbstractSimilarityTestCase;
 import com.jstarcraft.ai.math.algorithm.correlation.MathCorrelation;
-import com.jstarcraft.ai.math.algorithm.correlation.similarity.SpearmanRankCorrelation;
 
 public class SpearmanRankCorrelationTestCase extends AbstractSimilarityTestCase {
 
